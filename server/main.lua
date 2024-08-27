3f8fd514bc9951ba97a8cbf22ad4df5eca0bb077
MaDHouSe <madhouse1979@gmail.com>
2024-08-27 15:46:18 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,6 @@
 --[[ ===================================================== ]]--
 --[[             MH Intercom Script by MaDHouSe            ]]--
 --[[ ===================================================== ]]--
-
 RegisterServerEvent('mh-intercom:server:alertworkers')
 AddEventHandler('mh-intercom:server:alertworkers', function(job)
     TriggerClientEvent('mh-intercom:client:callintercom', -1, job)
