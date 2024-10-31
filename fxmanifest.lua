519c98dea189298a19c89ea0304bab55a48dea0f
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:31:31 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game 'gta5'
 
 author 'MaDHouSe'
 description 'MH Intercom'
-version '1.0'
+version '1.0.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
@@ -21,6 +21,7 @@ client_scripts {
 
 server_scripts {
     'server/main.lua',
+    'server/update.lua',
 }
 
 dependencies {
