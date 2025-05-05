f9b96823b37758c5beea38eb126c3f6a6a97b80d
MaDHouSe <madhouse1979@gmail.com>
2025-05-05 09:42:15 +0200

    Delete server directory

--- server/main.lua
+++
@@ -1,7 +0,0 @@
---[[ ===================================================== ]]--
---[[             MH Intercom Script by MaDHouSe            ]]--
---[[ ===================================================== ]]--
-RegisterServerEvent('mh-intercom:server:alertworkers')
-AddEventHandler('mh-intercom:server:alertworkers', function(job)
-    TriggerClientEvent('mh-intercom:client:callintercom', -1, job)
-end)
